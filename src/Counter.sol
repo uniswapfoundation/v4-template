88894d2c73169d6b215590641a8b50a295307d10
saucepoint <saucepoint@protonmail.com>
2023-06-13 15:01:16 -0400

    dead simple hook w/ test setup

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,46 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
-contract Counter {
-    uint256 public number;
+import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
+import {BaseHook} from "v4-periphery/BaseHook.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {BalanceDelta} from "@uniswap/v4-core/contracts/types/BalanceDelta.sol";
+
+contract Counter is BaseHook {
+    uint256 public beforeSwapCount;
+    uint256 public afterSwapCount;
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    function getHooksCalls() public pure override returns (Hooks.Calls memory) {
+        return Hooks.Calls({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeModifyPosition: false,
+            afterModifyPosition: false,
+            beforeSwap: true,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false
+        });
     }
 
-    function increment() public {
-        number++;
+    function beforeSwap(address, IPoolManager.PoolKey calldata, IPoolManager.SwapParams calldata)
+        external
+        override
+        returns (bytes4)
+    {
+        beforeSwapCount++;
+        return BaseHook.beforeSwap.selector;
+    }
+
+    function afterSwap(address, IPoolManager.PoolKey calldata, IPoolManager.SwapParams calldata, BalanceDelta)
+        external
+        override
+        returns (bytes4)
+    {
+        afterSwapCount++;
+        return BaseHook.afterSwap.selector;
     }
 }
---
+++ src/implementation/CounterImplementation.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {Counter} from "../Counter.sol";
+
+import {BaseHook} from "v4-periphery/BaseHook.sol";
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
+
+contract CounterImplementation is Counter {
+    constructor(IPoolManager poolManager, Counter addressToEtch) Counter(poolManager) {
+        Hooks.validateHookAddress(addressToEtch, getHooksCalls());
+    }
+
+    // make this a no-op in testing
+    function validateHookAddress(BaseHook _this) internal pure override {}
+}
