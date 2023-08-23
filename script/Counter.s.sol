e1bf5fa8af2ce0f25fd486e1700e1911d9551151
saucepoint <saucepoint@protonmail.com>
2023-08-23 11:39:00 -0400

    bump solidity version

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.19;
 
 import "forge-std/Script.sol";
 import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
