2a554fd946c726fc4d65964bbadd0d824deb15e9
saucepoint <saucepoint@protonmail.com>
2024-09-08 13:43:39 -0400

    fix posm signature change for key / ticks

--- test/utils/EasyPosm.sol
+++ test/utils/EasyPosm.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.21;
 
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {BalanceDelta, toBalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
 import {Currency, CurrencyLibrary} from "v4-core/src/types/Currency.sol";
 import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
-import {PositionConfig} from "v4-periphery/src/libraries/PositionConfig.sol";
 import {Actions} from "v4-periphery/src/libraries/Actions.sol";
 import {SafeCast} from "v4-core/src/libraries/SafeCast.sol";
+import {PositionInfo, PositionInfoLibrary} from "v4-periphery/src/libraries/PositionInfoLibrary.sol";
 
 /// @title Easy Position Manager
 /// @notice A library for abstracting Position Manager calldata
@@ -15,10 +16,20 @@ library EasyPosm {
     using CurrencyLibrary for Currency;
     using SafeCast for uint256;
     using SafeCast for int256;
+    using PositionInfoLibrary for PositionInfo;
+
+    /// @dev packing data to avoid stack too deep error
+    struct MintData {
+        uint256 balance0Before;
+        uint256 balance1Before;
+        bytes[] params;
+    }
 
     function mint(
         IPositionManager posm,
-        PositionConfig memory config,
+        PoolKey memory poolKey,
+        int24 tickLower,
+        int24 tickUpper,
         uint256 liquidity,
         uint256 amount0Max,
         uint256 amount1Max,
@@ -26,43 +37,43 @@ library EasyPosm {
         uint256 deadline,
         bytes memory hookData
     ) internal returns (uint256 tokenId, BalanceDelta delta) {
-        Currency currency0 = config.poolKey.currency0;
-        Currency currency1 = config.poolKey.currency1;
+        (Currency currency0, Currency currency1) = (poolKey.currency0, poolKey.currency1);
 
-        bytes[] memory params = new bytes[](2);
-        params[0] = abi.encode(config, liquidity, amount0Max, amount1Max, recipient, hookData);
-        params[1] = abi.encode(currency0, currency1);
-
-        uint256 balance0Before = currency0.balanceOf(address(this));
-        uint256 balance1Before = currency1.balanceOf(address(this));
+        MintData memory mintData = MintData({
+            balance0Before: currency0.balanceOf(address(this)),
+            balance1Before: currency1.balanceOf(address(this)),
+            params: new bytes[](2)
+        });
+        mintData.params[0] =
+            abi.encode(poolKey, tickLower, tickUpper, liquidity, amount0Max, amount1Max, recipient, hookData);
+        mintData.params[1] = abi.encode(currency0, currency1);
 
         // Mint Liquidity
         tokenId = posm.nextTokenId();
         posm.modifyLiquidities(
-            abi.encode(abi.encodePacked(uint8(Actions.MINT_POSITION), uint8(Actions.SETTLE_PAIR)), params), deadline
+            abi.encode(abi.encodePacked(uint8(Actions.MINT_POSITION), uint8(Actions.SETTLE_PAIR)), mintData.params),
+            deadline
         );
 
         delta = toBalanceDelta(
-            -(balance0Before - currency0.balanceOf(address(this))).toInt128(),
-            -(balance1Before - currency1.balanceOf(address(this))).toInt128()
+            -(mintData.balance0Before - currency0.balanceOf(address(this))).toInt128(),
+            -(mintData.balance1Before - currency1.balanceOf(address(this))).toInt128()
         );
     }
 
     function increaseLiquidity(
         IPositionManager posm,
         uint256 tokenId,
-        PositionConfig memory config,
         uint256 liquidityToAdd,
         uint256 amount0Max,
         uint256 amount1Max,
         uint256 deadline,
         bytes memory hookData
     ) internal returns (BalanceDelta delta) {
-        Currency currency0 = config.poolKey.currency0;
-        Currency currency1 = config.poolKey.currency1;
+        (Currency currency0, Currency currency1) = getCurrencies(posm, tokenId);
 
         bytes[] memory params = new bytes[](3);
-        params[0] = abi.encode(tokenId, config, liquidityToAdd, amount0Max, amount1Max, hookData);
+        params[0] = abi.encode(tokenId, liquidityToAdd, amount0Max, amount1Max, hookData);
         params[1] = abi.encode(currency0);
         params[2] = abi.encode(currency1);
 
@@ -88,7 +99,6 @@ library EasyPosm {
     function decreaseLiquidity(
         IPositionManager posm,
         uint256 tokenId,
-        PositionConfig memory config,
         uint256 liquidityToRemove,
         uint256 amount0Min,
         uint256 amount1Min,
@@ -96,11 +106,10 @@ library EasyPosm {
         uint256 deadline,
         bytes memory hookData
     ) internal returns (BalanceDelta delta) {
-        Currency currency0 = config.poolKey.currency0;
-        Currency currency1 = config.poolKey.currency1;
+        (Currency currency0, Currency currency1) = getCurrencies(posm, tokenId);
 
         bytes[] memory params = new bytes[](2);
-        params[0] = abi.encode(tokenId, config, liquidityToRemove, amount0Min, amount1Min, hookData);
+        params[0] = abi.encode(tokenId, liquidityToRemove, amount0Min, amount1Min, hookData);
         params[1] = abi.encode(currency0, currency1, recipient);
 
         uint256 balance0Before = currency0.balanceOf(address(this));
@@ -119,19 +128,17 @@ library EasyPosm {
     function collect(
         IPositionManager posm,
         uint256 tokenId,
-        PositionConfig memory config,
         uint256 amount0Min,
         uint256 amount1Min,
         address recipient,
         uint256 deadline,
         bytes memory hookData
     ) internal returns (BalanceDelta delta) {
-        Currency currency0 = config.poolKey.currency0;
-        Currency currency1 = config.poolKey.currency1;
+        (Currency currency0, Currency currency1) = getCurrencies(posm, tokenId);
 
         bytes[] memory params = new bytes[](2);
         // collecting fees is achieved by decreasing liquidity with 0 liquidity removed
-        params[0] = abi.encode(tokenId, config, 0, amount0Min, amount1Min, hookData);
+        params[0] = abi.encode(tokenId, 0, amount0Min, amount1Min, hookData);
         params[1] = abi.encode(currency0, currency1, recipient);
 
         uint256 balance0Before = currency0.balanceOf(recipient);
@@ -150,18 +157,16 @@ library EasyPosm {
     function burn(
         IPositionManager posm,
         uint256 tokenId,
-        PositionConfig memory config,
         uint256 amount0Min,
         uint256 amount1Min,
         address recipient,
         uint256 deadline,
         bytes memory hookData
     ) internal returns (BalanceDelta delta) {
-        Currency currency0 = config.poolKey.currency0;
-        Currency currency1 = config.poolKey.currency1;
+        (Currency currency0, Currency currency1) = getCurrencies(posm, tokenId);
 
         bytes[] memory params = new bytes[](2);
-        params[0] = abi.encode(tokenId, config, 0, amount0Min, amount1Min, hookData);
+        params[0] = abi.encode(tokenId, 0, amount0Min, amount1Min, hookData);
         params[1] = abi.encode(currency0, currency1, recipient);
 
         uint256 balance0Before = currency0.balanceOf(recipient);
@@ -175,5 +180,14 @@ library EasyPosm {
             (currency0.balanceOf(recipient) - balance0Before).toInt128(),
             (currency1.balanceOf(recipient) - balance1Before).toInt128()
         );
+    }
+
+    function getCurrencies(IPositionManager posm, uint256 tokenId)
+        internal
+        view
+        returns (Currency currency0, Currency currency1)
+    {
+        (PoolKey memory key,) = posm.getPoolAndPositionInfo(tokenId);
+        return (key.currency0, key.currency1);
     }
 }
