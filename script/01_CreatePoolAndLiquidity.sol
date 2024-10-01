079a3b132b915d661371ebc1a0a3a29bb0dfcafc
saucepoint <saucepoint@protonmail.com>
2024-10-01 12:30:07 -0400

    initial script for multicall create and add liquidity

---
+++ script/01_CreatePoolAndLiquidity.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import {PositionManager} from "v4-periphery/src/PositionManager.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
+import {Actions} from "v4-periphery/src/libraries/Actions.sol";
+import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
+import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+
+contract CreatePoolAndAddLiquidityScript is Script {
+    using CurrencyLibrary for Currency;
+
+    /////////////////////////////////////
+    // --- Parameters to Configure --- //
+    /////////////////////////////////////
+    // addresses of the contracts
+    PositionManager public posm = PositionManager(address(0x1111));
+    IHooks public hookContract = IHooks(address(0x2222));
+    address public token0 = address(0x3333); // use CurrencyLibrary.ADDRESS_ZERO for Native Token (Ether)
+    address public token1 = address(0x4444);
+
+    // --- pool configuration --- //
+    // fees paid by swappers that accrue to liquidity providers
+    uint24 lpFee = 3000; // 0.30%
+    int24 tickSpacing = 60;
+
+    // starting price of the pool, in sqrtPriceX96
+    uint160 startingPrice = 79228162514264337593543950336; // floor(sqrt(1) * 2^96)
+
+    // --- liquidity position configuration --- //
+    uint256 public token0Amount = 1e18;
+    uint256 public token1Amount = 1e18;
+
+    // range of the position
+    int24 tickLower = -600; // must be a multiple of tickSpacing
+    int24 tickUpper = 600;
+    // ------------------------------- //
+    /////////////////////////////////////
+
+    function run() external {
+        // sort the tokens!
+        address _token0 = uint160(token0) < uint160(token1) ? token0 : token1;
+        address _token1 = uint160(token0) < uint160(token1) ? token1 : token0;
+
+        Currency currency0 = Currency.wrap(_token0);
+        Currency currency1 = Currency.wrap(_token1);
+
+        PoolKey memory pool = PoolKey({
+            currency0: currency0,
+            currency1: currency1,
+            fee: lpFee,
+            tickSpacing: tickSpacing,
+            hooks: hookContract
+        });
+
+        bytes memory hookData = new bytes(0);
+
+        // --------------------------------- //
+
+        // Converts token amounts to liquidity units
+        uint128 liquidity = LiquidityAmounts.getLiquidityForAmounts(
+            startingPrice,
+            TickMath.getSqrtPriceAtTick(tickLower),
+            TickMath.getSqrtPriceAtTick(tickUpper),
+            token0Amount,
+            token1Amount
+        );
+
+        // slippage limits
+        uint256 amount0Max = token0Amount + 1 wei;
+        uint256 amount1Max = token1Amount + 1 wei;
+
+        (bytes memory actions, bytes[] memory mintParams) =
+            _mintLiquidityParams(pool, tickLower, tickUpper, liquidity, amount0Max, amount1Max, address(this), hookData);
+
+        // multicall parameters
+        bytes[] memory params = new bytes[](2);
+
+        // initialize pool
+        params[0] = abi.encodeWithSelector(posm.initializePool.selector, pool, startingPrice, hookData);
+
+        // mint liquidity
+        params[1] = abi.encodeWithSelector(
+            posm.modifyLiquidities.selector, abi.encode(actions, mintParams), block.timestamp + 20
+        );
+
+        // if the pool is an ETH pair, native tokens are to be transferred
+        uint256 valueToPass = currency0.isAddressZero() ? amount0Max : 0;
+
+        // multicall to atomically create pool & add liquidity
+        vm.broadcast();
+        posm.multicall{value: valueToPass}(params);
+    }
+
+    /// @dev helper function for encoding mint liquidity operation
+    /// @dev does NOT encode SWEEP, developers should take care when minting liquidity on an ETH pair
+    function _mintLiquidityParams(
+        PoolKey memory poolKey,
+        int24 _tickLower,
+        int24 _tickUpper,
+        uint256 liquidity,
+        uint256 amount0Max,
+        uint256 amount1Max,
+        address recipient,
+        bytes memory hookData
+    ) internal pure returns (bytes memory, bytes[] memory) {
+        bytes memory actions = abi.encodePacked(uint8(Actions.MINT_POSITION), uint8(Actions.SETTLE_PAIR));
+
+        bytes[] memory params = new bytes[](2);
+        params[0] = abi.encode(poolKey, _tickLower, _tickUpper, liquidity, amount0Max, amount1Max, recipient, hookData);
+        params[1] = abi.encode(poolKey.currency0, poolKey.currency1);
+        return (actions, params);
+    }
+}
