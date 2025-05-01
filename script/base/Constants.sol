5eef0bdd376a05baadb144cc158707d69ee5bf31
Babuaravind Gururaj <gururaj.b@northeastern.edu>
2025-05-01 15:47:13 -0400

    removed the dead code

--- script/base/Constants.sol
+++ script/base/Constants.sol
@@ -18,10 +18,6 @@ contract Constants {
     address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
     IAllowanceTransfer constant PERMIT2 = IAllowanceTransfer(address(0x000000000022D473030F116dDEE9F6B43aC78BA3));
 
-    /// @dev populated with default anvil addresses - use for local development
-    // IPoolManager constant POOLMANAGER = IPoolManager(address(0x5FbDB2315678afecb367f032d93F642f64180aa3));
-    // PositionManager constant posm = PositionManager(payable(address(0x429ba70129df741B2Ca2a85BC3A2a3328e5c09b4)));
-
     constructor() {
         POOLMANAGER = IPoolManager(block.chainid.getPoolManagerByChainId());
         posm = PositionManager(payable(block.chainid.getPositionManagerByChainId()));
