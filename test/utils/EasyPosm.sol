af7ef28fda6e0189d128dc1896683a63da1ea92d
baxelson12 <bradaxelson@gmail.com>
2025-06-12 02:56:59 -0400

    fix: Library must check whether we are in test or scripting context.  Library code is copied to the caller's bytecode and script contracts are ephemeral so we can't use address(this) in them.  Integrate ContextDetection library

--- test/utils/EasyPosm.sol
+++ test/utils/EasyPosm.sol
@@ -8,6 +8,7 @@ import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol
 import {Actions} from "v4-periphery/src/libraries/Actions.sol";
 import {SafeCast} from "v4-core/src/libraries/SafeCast.sol";
 import {PositionInfo, PositionInfoLibrary} from "v4-periphery/src/libraries/PositionInfoLibrary.sol";
+import {ContextDetection} from "./ContextDetection.sol";
 
 /// @title Easy Position Manager
 /// @notice A library for abstracting Position Manager calldata
@@ -39,11 +40,12 @@ library EasyPosm {
         uint256 deadline,
         bytes memory hookData
     ) internal returns (uint256 tokenId, BalanceDelta delta) {
+        address context = ContextDetection.isTest() ? address(this) : address(posm);
         (Currency currency0, Currency currency1) = (poolKey.currency0, poolKey.currency1);
 
         MintData memory mintData = MintData({
-            balance0Before: currency0.balanceOf(address(this)),
-            balance1Before: currency1.balanceOf(address(this)),
+            balance0Before: currency0.balanceOf(context),
+            balance1Before: currency1.balanceOf(context),
             params: new bytes[](2)
         });
         mintData.params[0] =
@@ -59,8 +61,8 @@ library EasyPosm {
         );
 
         delta = toBalanceDelta(
-            -(mintData.balance0Before - currency0.balanceOf(address(this))).toInt128(),
-            -(mintData.balance1Before - currency1.balanceOf(address(this))).toInt128()
+            -(mintData.balance0Before - currency0.balanceOf(context)).toInt128(),
+            -(mintData.balance1Before - currency1.balanceOf(context)).toInt128()
         );
     }
 
@@ -73,6 +75,7 @@ library EasyPosm {
         uint256 deadline,
         bytes memory hookData
     ) internal returns (BalanceDelta delta) {
+        address context = ContextDetection.isTest() ? address(this) : address(posm);
         (Currency currency0, Currency currency1) = getCurrencies(posm, tokenId);
 
         bytes[] memory params = new bytes[](3);
@@ -80,8 +83,8 @@ library EasyPosm {
         params[1] = abi.encode(currency0);
         params[2] = abi.encode(currency1);
 
-        uint256 balance0Before = currency0.balanceOf(address(this));
-        uint256 balance1Before = currency1.balanceOf(address(this));
+        uint256 balance0Before = currency0.balanceOf(context);
+        uint256 balance1Before = currency1.balanceOf(context);
 
         uint256 valueToPass = currency0.isAddressZero() ? amount0Max : 0;
         posm.modifyLiquidities{value: valueToPass}(
@@ -95,8 +98,8 @@ library EasyPosm {
         );
 
         delta = toBalanceDelta(
-            (currency0.balanceOf(address(this)).toInt256() - balance0Before.toInt256()).toInt128(),
-            (currency1.balanceOf(address(this)).toInt256() - balance1Before.toInt256()).toInt128()
+            (currency0.balanceOf(context).toInt256() - balance0Before.toInt256()).toInt128(),
+            (currency1.balanceOf(context).toInt256() - balance1Before.toInt256()).toInt128()
         );
     }
 
@@ -110,22 +113,23 @@ library EasyPosm {
         uint256 deadline,
         bytes memory hookData
     ) internal returns (BalanceDelta delta) {
+        address context = ContextDetection.isTest() ? address(this) : address(posm);
         (Currency currency0, Currency currency1) = getCurrencies(posm, tokenId);
 
         bytes[] memory params = new bytes[](2);
         params[0] = abi.encode(tokenId, liquidityToRemove, amount0Min, amount1Min, hookData);
         params[1] = abi.encode(currency0, currency1, recipient);
 
-        uint256 balance0Before = currency0.balanceOf(address(this));
-        uint256 balance1Before = currency1.balanceOf(address(this));
+        uint256 balance0Before = currency0.balanceOf(context);
+        uint256 balance1Before = currency1.balanceOf(context);
 
         posm.modifyLiquidities(
             abi.encode(abi.encodePacked(uint8(Actions.DECREASE_LIQUIDITY), uint8(Actions.TAKE_PAIR)), params), deadline
         );
 
         delta = toBalanceDelta(
-            (currency0.balanceOf(address(this)) - balance0Before).toInt128(),
-            (currency1.balanceOf(address(this)) - balance1Before).toInt128()
+            (currency0.balanceOf(context) - balance0Before).toInt128(),
+            (currency1.balanceOf(context) - balance1Before).toInt128()
         );
     }
 
